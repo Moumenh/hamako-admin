2bec114b1398af03f2016d3c2774e4e970d9bef7
Momen Hanbali <momen.hanbali@xngage.com>
2022-12-26 00:18:59 +0200

    default value & response

--- src/Modules/Products/new-product-form.tsx
+++ src/Modules/Products/new-product-form.tsx
@@ -11,7 +11,7 @@ const NewProductForm = () => {
         <Input type="text" placeholder="Product Slug" mb="4" {...register("slug", { required: true })} />
         <Input type="text" placeholder="Product description" mb="4" {...register("description", { minLength: 6 })} />
         <Box mb="4">
-          <Checkbox {...register("isActive", { value: "true" })} mr="4">
+          <Checkbox {...register("isActive", { value: true } as any)} mr="4">
             is Active
           </Checkbox>
           <Checkbox {...register("isVariant")}>is variant Product</Checkbox>
